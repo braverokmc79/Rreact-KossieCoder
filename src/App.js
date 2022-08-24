09ba75cd63e3688b03872a82426b061c37fc1693
braverokmc79 <braverokmc79@gmail.com>
2022-08-25 05:25:32 +0900

    12 영화 리스트 추가 폼 만들기 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import Movie from './components/Movie';
+import { useState } from 'react';
 
 
 function App() {
-  const movies = [
-    { id: 1, title: 'kossie coder1', year: 2001 },
-    { id: 2, title: 'kossie coder2', year: 2002 },
-    { id: 3, title: 'kossie coder3', year: 2003 },
-    { id: 4, title: 'kossie coder4', year: 2004 },
-  ]
+
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [maxId, setMaxId] = useState(5);
+
+
+  const [movies, setMovies] = useState(
+    [
+      { id: 1, title: 'kossie coder1', year: 2001 },
+      { id: 2, title: 'kossie coder2', year: 2002 },
+      { id: 3, title: 'kossie coder3', year: 2003 },
+      { id: 4, title: 'kossie coder4', year: 2004 },
+    ]
+  );
+
 
   const renderMovies = movies.map((movie) => {
     return <Movie key={movie.id} movie={movie} />
   })
 
+  const addMovie = (event) => {
+    setMaxId(maxId + 1);
+
+    event.preventDefault();
+
+    setMovies([...movies, {
+      id: maxId,
+      title: movieTitle,
+      year: movieYear
+    }])
+
+  }
+
   return (
     <div className="App">
       <h1>Movie list</h1>
+      <form onSubmit={addMovie}>
+        <input
+          type="text"
+          value={movieTitle}
+          placeholder='영화제목'
+          onChange={e => setMovieTitle(e.target.value)}
+        /><br />
+        <input
+          type="text"
+          value={movieYear}
+          placeholder='개봉년도'
+          onChange={e => setMovieYear(e.target.value)}
+        /><br /><br />
+
+        <button>영화추가</button>
+      </form><br /><br />
       {renderMovies}
     </div >
   );
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Movie = ({ movie }) => {
 
     return (
-        <div className='movie'>
+        <div className='movie' id={movie.id}>
             <div className='movie-title'>{movie.title}</div>
             <div className='movie-year'>{movie.year}</div>
         </div>
