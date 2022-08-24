5e30df51c48b2148a02cd1da8a068f8fe26bbdd8
braverokmc79 <braverokmc79@gmail.com>
2022-08-25 04:56:29 +0900

    11. 컴포넌트와 props 데이터 보내기 복습

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Movie from './components/Movie';
 
 
@@ -7,26 +6,17 @@ function App() {
     { id: 1, title: 'kossie coder1', year: 2001 },
     { id: 2, title: 'kossie coder2', year: 2002 },
     { id: 3, title: 'kossie coder3', year: 2003 },
+    { id: 4, title: 'kossie coder4', year: 2004 },
   ]
 
   const renderMovies = movies.map((movie) => {
-    return (
-      <div className='movie' key={movie.id}>
-        <div className='movie-title'>{movie.title}</div>
-        <div className='movie-year'>{movie.year}</div>
-      </div >
-    );
+    return <Movie key={movie.id} movie={movie} />
   })
-
 
   return (
     <div className="App">
       <h1>Movie list</h1>
       {renderMovies}
-      {/* {movies.map((movie) => {
-        return <Movie key={movie.id} movie={movie} />
-      })} */}
-
     </div >
   );
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Movie = (props) => {
-    const { title, year } = props.movie;
+const Movie = ({ movie }) => {
 
     return (
         <div className='movie'>
-            <div className='movie-title'>{title}</div>
-            <div className='movie-year'>{year}</div>
+            <div className='movie-title'>{movie.title}</div>
+            <div className='movie-year'>{movie.year}</div>
         </div>
     );
 };
