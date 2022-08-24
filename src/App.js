bbc0f32f4769af8ea77777838a16e8d09a24aea3
braverokmc79 <braverokmc79@gmail.com>
2022-08-25 01:20:46 +0900

    06 useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [count, setCount] = useState(0);
+  const [kossie, setKossie] = useState(0);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    alert("submitted");
-  }
+  useEffect(() => {
+    console.log("count :", count);
+  }, [count]);
 
-  const handleForm = (e) => {
-    setNickname(e.target.value)
-    console.log("handleForm : ", [e.target.name], e.target.value);
+
+  useEffect(() => {
+    console.log("first rendering");
+  }, []);
+
+  const increment = () => {
+    setCount(count + 1);
   }
 
   return (
     <div className="App">
-
-      <form onSubmit={onSubmit}>
-        <input placeholder='Username'
-          name='username'
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value)
-          }} /><br />
-
-
-        <input placeholder='Password'
-          name='username'
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value)
-          }}
-        /><br />
-
-        <input placeholder='Nickname'
-          name='nickname'
-          value={nickname}
-          onChange={handleForm}
-        /><br />
-
-        <button>Login</button>
-      </form>
-
+      <h1>hong gil dong</h1>
+      <button onClick={increment}>Click</button>
+      <button onClick={() => {
+        console.log("kossie", kossie);
+        setKossie(kossie + 1);
+      }}>Click1</button>
     </div >
   );
 
