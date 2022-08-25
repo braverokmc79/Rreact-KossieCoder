d79b5e30caa5b33618a874a0356d4cbc95497991
braverokmc79 <braverokmc79@gmail.com>
2022-08-25 21:50:35 +0900

    25.유저 데이터 화면에 출력하기 2

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PageMovies from './pages/PageMovies';
 import Users from './pages/Users';
 
-
-
 function App() {
 
   const [movies, setMovies] = useState([]);
@@ -15,7 +13,6 @@ function App() {
   const removeMovie = (id) => {
     setMovies(movies.filter((movie) => movie.id !== id));
   }
-
   const renderMovies = movies.length ? movies.map((movie) => {
     return <Movie key={movie.id} movie={movie} removeMovie={removeMovie} />
   }) : "추가된 영화가 없습니다. ";
@@ -26,22 +23,17 @@ function App() {
   }
 
   return (
-
-
     <div className="App">
-      <Navbar />
-
-      <Routes>
-        <Route path='/' element={<h1>Home</h1>}></Route>
-        <Route path='/movies' element={<PageMovies addMovie={addMovie} renderMovies={renderMovies} />}></Route>
-        <Route path='/users' element={<Users />}></Route>
-      </Routes>
+      <div className='container'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>}></Route>
+          <Route path='/movies' element={<PageMovies addMovie={addMovie} renderMovies={renderMovies} />}></Route>
+          <Route path='/users' element={<Users />}></Route>
+        </Routes>
+      </div>
 
     </div >
-
-
   );
-
-
 }
 export default App;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,14 +4,18 @@ const UserList = ({ users }) => {
     console.log(users);
     return (
         <div>
-            {users.map((user) => {
-                return (
-                    <div key={user.id}>
-                        {user.name}
-                    </div>
-                );
-            })}
-        </div>
+            {
+                users.map((user) => {
+                    return (
+                        <div className="card mb-2" key={user.id}>
+                            <div className="card-body p-3">
+                                {user.name}
+                            </div>
+                        </div>
+                    );
+                })
+            }
+        </div >
     );
 };
 
