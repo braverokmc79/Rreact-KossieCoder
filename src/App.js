4753f20f243d6534763e95a9028e83573ee9e5a0
braverokmc79 <braverokmc79@gmail.com>
2022-08-25 22:16:14 +0900

    27. Home/Movies 페이지 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
-import Movie from './components/Movie';
-import { useState } from 'react';
-import MovieForm from './components/MovieForm';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PageMovies from './pages/PageMovies';
+import { Routes, Route } from 'react-router-dom';
+import Movies from './pages/Movies';
 import Users from './pages/Users';
+import Home from './pages/Home';
 
 function App() {
-
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(movies.filter((movie) => movie.id !== id));
-  }
-  const renderMovies = movies.length ? movies.map((movie) => {
-    return <Movie key={movie.id} movie={movie} removeMovie={removeMovie} />
-  }) : "추가된 영화가 없습니다. ";
-
-
-  const addMovie = (movie) => {
-    setMovies([...movies, movie]);
-  }
 
   return (
     <div className="App">
       <div className='container'>
         <Navbar />
         <Routes>
-          <Route path='/' element={<h1>Home</h1>}></Route>
-          <Route path='/movies' element={<PageMovies addMovie={addMovie} renderMovies={renderMovies} />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/movies' element={<Movies />}></Route>
           <Route path='/users' element={<Users />}></Route>
         </Routes>
       </div>
---
+++ src/pages/Movies.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import MovieForm from '../components/MovieForm';
+import Movie from './../components/Movie';
+
+const PageMovies = () => {
+
+    const [movies, setMovies] = useState([]);
+
+    const removeMovie = (id) => {
+        setMovies(movies.filter((movie) => movie.id !== id));
+    }
+    const renderMovies = movies.length ? movies.map((movie) => {
+        return <Movie key={movie.id} movie={movie} removeMovie={removeMovie} />
+    }) : "추가된 영화가 없습니다. ";
+
+
+    const addMovie = (movie) => {
+        setMovies([...movies, movie]);
+    }
+
+
+    return (
+        <div>
+            <h1>Movie list</h1>
+            <MovieForm addMovie={addMovie} />
+            <br /><br />
+            {renderMovies}
+        </div>
+    );
+};
+
+export default PageMovies;
